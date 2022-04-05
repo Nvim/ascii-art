121f00c74d03808d3dca687c1b24e5625aaec451
Nvim <naimssj4@gmail.com>
2022-04-05 22:43:30 +0200

    la c'est mieux
    ça fait les 2 headers en structs (clean)

--- fopen.c
+++ fopen.c
@@ -1,70 +1,54 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-//affiche les infos du header (14 premiers octets):
-void print_header(char *img){
+struct bmp_header{
+    char name[2];//BM
+    int size;//taille de l'image au total (headers inclus)
+    short int app1;//on s'en fout mais c'est 0
+    short int app2;//pareil
+    int offset; //octet du premier pixel
+};
 
-    FILE * p = p = fopen(img,"rb");
-    if (p == NULL){
+struct dib_header{
+    int header_size;//taille du header dib (40 octets)
+    int width;
+    int height;
+    short int colorplanes;//pas compris mais c'est 1
+    short int bpp;//bits par pixel, 8 = noir et blanc 24 = RGB
+    int compression;//méthode de compression utilisée
+    int img_size;//taille de l'image (headers exclus)
+};
+
+void print_header_structs(char *img){
+    FILE * fp = fopen(img,"rb");
+    if (fp == NULL){
         printf("\nerreur");
         exit(EXIT_FAILURE);
     }
-    printf("\nouverture reussie");
+    printf("\nouverture reussie\n");
+    struct bmp_header bmp_header;
+    struct dib_header dib_header;
     
-    //2 premiers octets: char 'B' et 'M'
-    char octet_1;
-    char octet_2;
-    fread(&octet_1,1,1,p);
-    fread(&octet_2,1,1,p);
+    //on lit pas tout le struct d'un coup a cause du nom en char
+    fread(bmp_header.name,2,1,fp);
+    printf("\nType d'image: %c%c", bmp_header.name[0],bmp_header.name[1]);
 
-    //octets 2 à 6: taille de l'image
-    int taille_image;
-    fread(&taille_image,4,1,p);
+    //on devrait lire tout ça d'un coup avec 2*sizeof(int)+2*sizeof(short int) = 3*sizeof(int)
+    fread(&bmp_header.size,4,1,fp);
+    fread(&bmp_header.app1,2,1,fp);
+    fread(&bmp_header.app2,2,1,fp);
+    fread(&bmp_header.offset,4,1,fp);
+    printf("\nTaille de l'image (header inclus): %d\nOffset (nombre d'octets avant le premier pixel): %d\n",bmp_header.size,bmp_header.offset);
 
-    //octets 6 à 10: application utilisée(0) 2 fois
-    int application;
-    int application2;
-    fread(&application,2,1,p);
-    fread(&application2,2,1,p);
 
-    //octets 10 à 14: adresse de l'octet ou les données de l'image commencent
-    int debut;
-    fread(&debut,4,1,p);
+    //méthode lire tout le struct d'un coup
+    fread(&dib_header,sizeof(struct dib_header),1,fp);
+    printf("\nTaille du header DIB: %d\nLargeur: %dpx\nHauteur: %dpx\nColor Planes: %d\nBits par pixel: %d\nMethode de compression: %d\nTaille de l'image (sans headers): %d", dib_header.header_size, dib_header.width,dib_header.height,dib_header.colorplanes,dib_header.bpp,dib_header.compression,dib_header.img_size);
+}
 
-    //octets 14 à 18: taille du header 2
-    int size;
-    fread(&size,4,1,p);
-
-    //octets 18 à 20: width
-    int width;
-    fread(&width,2,1,p);
-    //octets 20 à 22: height
-    int height;
-    fread(&height,2,1,p);
-    //octets 22 à 24: color planes(must be 1)
-    int planes;
-    fread(&planes,2,1,p);
-    //octets 24 à 26: bits par pixel
-    int bits;
-    fread(&bits,2,1,p);
-
-    //affichage
-    printf("\ntype de l'image: %c%c", octet_1,octet_2);
-    printf("\ntaille de l'image: %d octets", taille_image);
-    printf("\napplication utilisee: %d", application);
-    printf("\n0 normalement: %d", application2);
-    printf("\noctet du premier pixel: %d\n~~~~~~~~", debut);
-    printf("\ntaille du header 2: %d",size);
-    printf("\nlargeur de l'image: %d",width);
-    printf("\nhauteur de l'image: %d",height);
-    printf("\ncolor planes: %d",planes);
-    printf("\nnombre de bits par pixel: %d",bits);
-    fclose(p);
-
-}    
 
 int main(void){
 
-    print_header("minecraft.bmp");
+    print_header_structs("minecraft.bmp");
     return EXIT_SUCCESS;
 }
