7680e32251a00eb13523474861107c8d788ef437
Nvim <naimssj4@gmail.com>
2022-04-06 22:54:50 +0200

    06/04
    remplacé les int par des unsigned?
    ajouté des images 256 et 16 couleurs pour tester
    -> offset différent selon les types ?
    ajouté une vérification de la taille des variables avant l'exécution

--- fopen.c
+++ fopen.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-struct bmp_header{
-    char name[2];//BM
-    int size;//taille de l'image au total (headers inclus)
-    short int app1;//on s'en fout mais c'est 0
-    short int app2;//pareil
-    int offset; //octet du premier pixel
+struct bmp_header{ //correspond aux 14 premiers octets
+    unsigned char name[2];//BM
+    unsigned int size;//taille de l'image au total (headers inclus)
+    unsigned short int app1;//on s'en fout mais c'est 0
+    unsigned short int app2;//pareil
+    unsigned int offset; //octet du premier pixel: 54 pour les 24bits(16 777 216 couleurs), 1078 pour les 8bits(256 couleurs) 118 pour les 4bits(16 couleurs)
 };
 
-struct dib_header{
-    int header_size;//taille du header dib (40 octets)
-    int width;
-    int height;
-    short int colorplanes;//pas compris mais c'est 1
-    short int bpp;//bits par pixel, 8 = noir et blanc 24 = RGB
-    int compression;//méthode de compression utilisée
-    int img_size;//taille de l'image (headers exclus)
+struct dib_header{ //correspond aux 40 premiers octets noramlement
+    unsigned int header_size;//taille du header dib (40 octets)
+    unsigned int width;
+    unsigned int height;
+    unsigned short int colorplanes;//pas compris mais c'est 1
+    unsigned short int bpp;//bits par pixel, 8 = noir et blanc 24 = RGB
+    unsigned int compression;//méthode de compression utilisée
+    unsigned int img_size;//taille de l'image (headers exclus)
 };
 
+
+//affiche les informations utiles des headers BMP et DIB
 void print_header_structs(char *img){
     FILE * fp = fopen(img,"rb");
     if (fp == NULL){
         printf("\nerreur");
         exit(EXIT_FAILURE);
     }
-    printf("\nouverture reussie\n");
+    printf("\nOuverture reussie: \n");
     struct bmp_header bmp_header;
     struct dib_header dib_header;
     
@@ -33,7 +35,7 @@ void print_header_structs(char *img){
     fread(bmp_header.name,2,1,fp);
     printf("\nType d'image: %c%c", bmp_header.name[0],bmp_header.name[1]);
 
-    //on devrait lire tout ça d'un coup avec 2*sizeof(int)+2*sizeof(short int) = 3*sizeof(int)
+    //on devrait lire tout ça d'un coup avec 2*sizeof(int)+2*sizeof(short int) = 3*sizeof(int) ?
     fread(&bmp_header.size,4,1,fp);
     fread(&bmp_header.app1,2,1,fp);
     fread(&bmp_header.app2,2,1,fp);
@@ -49,6 +51,12 @@ void print_header_structs(char *img){
 
 int main(void){
 
-    print_header_structs("minecraft.bmp");
-    return EXIT_SUCCESS;
+    if(sizeof(char) == 1 && sizeof(short int) == 2 && sizeof(int) == 4){
+        print_header_structs("onepiece256.bmp");
+        return EXIT_SUCCESS;
+    }
+    else{
+        printf("\nTaille des variables pas adaptée");
+        return EXIT_FAILURE;
+    }
 }
